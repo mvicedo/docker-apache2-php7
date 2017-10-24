50f94578c19145180e9c0396f6ca3c05f814856c
Miguel Angel Vicedo <miguel.angel.vicedo@accenture.com>
2017-10-24 12:12:52 +0200

    Test

--- dist/test.php
+++ dist/test.php
@@ -4,6 +4,18 @@
 
 <?php
 echo "My first PHP script!";
+
+ 
+$foo = 8;
+ 
+if( $foo<10 )
+    if( $foo>5 )
+        echo "Greater than 5!";
+    else
+        echo "Less than 5!";
+else
+    echo "Greater than 10!";
+    echo "<br />Another note.";
 ?>
 
 </body>
