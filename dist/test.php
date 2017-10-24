e82ff649e3e683a9a3a3503503eb68585766c6da
Miguel Angel Vicedo <miguel.angel.vicedo@accenture.com>
2017-10-24 12:30:35 +0200

    Test

--- dist/test.php
+++ dist/test.php
@@ -4,18 +4,6 @@
 
 <?php
 echo "My first PHP script!";
-
- 
-$foo = 8;
- 
-if( $foo<10 )
-    if( $foo>5 )
-        echo "Greater than 5!";
-    else
-        echo "Less than 5!";
-else
-    echo "Greater than 10!";
-    echo "<br />Another note.";
 ?>
 
 </body>
